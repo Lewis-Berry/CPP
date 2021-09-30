084d7bf969f166447d54ec77af9ad01d8b954646
Lewis Berry <lewisberry18@yahoo.com>
2021-09-30 18:46:49 +0100

    Basic functionality added.
    Added functionality to accept basic user inputs for displayed options.

--- atm.cpp
+++ atm.cpp
@@ -15,8 +15,37 @@ void showMenu() {
 int main() {
 	
 	//show menu, show balance, deposit, withdraw
+	int userSelection;
+	double balance = 500;
 
-	showMenu();
+	//creating do-while loop for application
+	do {
+		showMenu();
+		cout << "Select an option: " << endl;
+		cin >> userSelection;
+		cout << " " << endl;
+
+		//handling user selection with switch case
+		switch (userSelection) {
+		case 1: cout << "Your Balance is: " << balance << endl;
+			break;
+		case 2: cout << "Desposit Amount: ";
+			double depositAmount;
+			cin >> depositAmount;
+			balance += depositAmount;
+			break;
+		case 3: cout << "Withdraw Amount: ";
+			double withdrawAmount;
+			cin >> withdrawAmount;
+			if (withdrawAmount <= balance)
+				balance -= withdrawAmount;
+			else
+				cout << "Insufficient balance to action request!" << endl;
+			break;
+
+
+		}
+	} while (userSelection != 4);
 
 
 
