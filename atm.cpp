8d063be27ed508e9ddcbe5ca6cbd51f638b0419c
Lewis Berry <lewisberry18@yahoo.com>
2021-09-30 18:00:06 +0100

    ATM Menu Created
    Draft ATM Menu function created.

--- atm.cpp
+++ atm.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+using namespace std;
+
+
+void showMenu() {
+	cout << "********** MENU **********" << endl;
+	cout << "1. Check Balance" << endl;
+	cout << "2. Desposit." << endl;
+	cout << "3. Withdraw" << endl;
+	cout << "4. Exit" << endl;
+	cout << "**************************" << endl;
+}
+
+
 int main() {
-	std::cout << "Hello World!" << endl;
+	
+	//show menu, show balance, deposit, withdraw
+
+	showMenu();
+
+
+
 	return 0;
 }
